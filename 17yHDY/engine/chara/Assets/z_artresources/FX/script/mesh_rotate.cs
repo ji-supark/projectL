032f3683d73ad4b298b1af820a14c226c26656f7
ji-supark <dexignerke@gmail.com>
2017-05-05 21:09:43 +0900

    이펙트작업시작

---
+++ 17yHDY/engine/chara/Assets/z_artresources/FX/script/mesh_rotate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mesh_rotate : MonoBehaviour {       
+	public float myRotationSpeed = 100.0f;
+
+	public bool isRotateX = false;
+	public bool isRotateY = false;
+	public bool isRotateZ = false;
+
+	private bool positiveRotation = true;
+
+	private int posOnNeg = 1;
+
+	void Start () {
+
+				GetComponent<Collider>().isTrigger = true;
+				if (positiveRotation == false) {
+						posOnNeg = -1;
+
+				}
+		}
+
+	void Update () {
+
+				if (isRotateX) {
+						transform.Rotate (myRotationSpeed * Time.deltaTime * posOnNeg, 0, 0);
+				} else if (isRotateY) {
+						transform.Rotate (0, myRotationSpeed * Time.deltaTime * posOnNeg, 0);
+				} else if (isRotateZ) {
+						transform.Rotate (0, 0, myRotationSpeed * Time.deltaTime * posOnNeg);
+				}
+		}
+}
+
